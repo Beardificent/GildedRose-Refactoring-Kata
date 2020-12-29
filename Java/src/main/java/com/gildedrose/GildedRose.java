3b7d95a137a499ee25fe1d0db3dc1ea44ebc2bc2
Beardificent <cis.magito@gmail.com>
2020-12-29 11:50:05 +0100

    switched to a switch case as originally planned!

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -7,33 +7,43 @@ class GildedRose {
         this.items = items;
     }
 
+    //used extract method to get the entire if conditional body::
+    //makes it cleaner to read the updateQuality method.
     public void updateQuality() {
         for (Item item : items) {
             checkConditionsUpdateQuality(item);
         }
     }
 
+
+
     private void checkConditionsUpdateQuality(Item item) {
-        if (item.name.equals("Aged Brie")) {
+        //Copy conditional to clipboard.
+        //Select entire else body:: EXTRACT METHOD to whatevername (foo eg)::
+        //SURROUND with IF/ELSE
+        //COPY method to both conditions.
+        //INLINE IT.
+        switch (item.name) {
+            case "Aged Brie":
                 if (item.quality < 50) {
                     item.quality = item.quality + 1;
 
                 }
-            item.sellIn = item.sellIn - 1;
+                item.sellIn = item.sellIn - 1;
 
-            if (item.sellIn < 0) {
-                if (item.quality < 50) {
-                    item.quality = item.quality + 1;
-                }
-            }
-        } else {
-            if (!item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                if (item.quality > 0) {
-                    if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-                        item.quality = item.quality - 1;
+                if (item.sellIn < 0) {
+                    if (item.quality < 50) {
+                        item.quality = item.quality + 1;
                     }
                 }
-            } else {
+                break;
+            case "Backstage passes to a TAFKAL80ETC concert":
+
+                //Copy conditional to clipboard.
+                //Select entire else body:: EXTRACT METHOD to whatevername::
+                //SURROUND with IF/ELSE
+                //COPY method to both conditions.
+                //INLINE IT.
                 if (item.quality < 50) {
                     item.quality = item.quality + 1;
                     if (item.sellIn < 11) {
@@ -47,23 +57,32 @@ class GildedRose {
                         }
                     }
                 }
-            }
-            if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
                 item.sellIn = item.sellIn - 1;
-            }
 
-            if (item.sellIn < 0) {
-                if (!item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                    if (item.quality > 0) {
-                        if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-                            item.quality = item.quality - 1;
-                        }
-                    }
-                } else {
+                if (item.sellIn < 0) {
                     item.quality = 0;
                 }
+                break;
+            case "Sulfuras, Hand of Ragnaros":
+                //Copy conditional to clipboard.
+                //Select entire else body:: EXTRACT METHOD to whatevername::
+                //SURROUND with IF/ELSE
+                //COPY method to both conditions.
+                //INLINE IT.
+
+                break;
+            default:
+                if (item.quality > 0) {
+                    item.quality = item.quality - 1;
+                }
+                item.sellIn = item.sellIn - 1;
+
+                if (item.sellIn < 0) {
+                    if (item.quality > 0) {
+                        item.quality = item.quality - 1;
+                    }
+                }
+                break;
+        }
             }
         }
-    }
-
-}
